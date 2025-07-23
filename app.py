85ab5933e44e40330d7ccc372f960f5284e47781
Vrithik Prince <69463172+vrithikprince@users.noreply.github.com>
2025-07-23 15:29:21 +0530

    app.py

--- app.py
+++ app.py
@@ -187,4 +187,5 @@ def predict():
 
 # ------------------------ Run Flask App ------------------------
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
